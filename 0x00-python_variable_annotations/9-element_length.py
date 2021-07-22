4e44b452ca319a2add303f9b0acc824ff20996d1
Ubuntu <ubuntu@ip-172-31-7-225.us-east-2.compute.internal>
2021-07-22 22:48:29 +0000

    Edit: add comment after def line

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -9,4 +9,5 @@ typeAlias = List[Tuple[Sequence, int]]
 
 
 def element_length(lst: Iterable[Sequence]) -> typeAlias:
+    """returns values with appropriate types"""
     return [(i, len(i)) for i in lst]
