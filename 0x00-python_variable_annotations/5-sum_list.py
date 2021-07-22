d8dc3f41f46cc247cdcf850482631529e22d435e
Ubuntu <ubuntu@ip-172-31-7-225.us-east-2.compute.internal>
2021-07-22 21:33:29 +0000

    Edit: change list type checking to float instead of int

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -6,7 +6,7 @@ argument and returns their sum as a float
 from typing import List
 
 
-def sum_list(input_list: List[int]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """return sum of argument list elements"""
     total: float = 0.0
     for i in range(len(input_list)):
