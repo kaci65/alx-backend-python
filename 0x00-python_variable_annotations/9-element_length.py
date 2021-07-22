8ecd46d54a0eb0a8df4bec332617de5f1a4ae4d3
Ubuntu <ubuntu@ip-172-31-7-225.us-east-2.compute.internal>
2021-07-22 22:44:09 +0000

    First commit

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Annotate the function’s parameters and return values with the appropriate types
+"""
+from typing import Sequence, List, Tuple, Iterable
+
+
+typeAlias = List[Tuple[Sequence, int]]
+
+
+def element_length(lst: Iterable[Sequence]) -> typeAlias:
+    return [(i, len(i)) for i in lst]
